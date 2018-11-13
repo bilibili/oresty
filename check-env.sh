3004fddc65b55c81e03999cbcb7a3f683a412789
tangjunxing <tangjunxing@bilibili.com>
2018-11-13 14:43:06 +0800

    Mod

--- check-env.sh
+++ check-env.sh
@@ -3,7 +3,7 @@ set -e
 
 dpkg -i ./oresty_1.13.6.2-20180626105705_amd64.deb
 
-cp -r ./lualib/ /usr/local/oresty/site/
+mkdir -p /usr/local/oresty/site/ && cp -r ./lualib/ /usr/local/oresty/site/
 
 name=${1:-oresty}
 sed "s@_APP_DIR_@$PWD@;s@_NAME_@$name@" oresty.template > $name
