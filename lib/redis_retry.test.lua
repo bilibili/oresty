ef4bcd5584c44d07c7248054f3965be1613a73d3
tangjunxing <tangjunxing@bilibili.com>
2020-03-13 17:56:03 +0800

    Mod

--- lib/redis_retry.test.lua
+++
@@ -1,7 +0,0 @@
-local serpent = require 'serpent'
-local redis_retry = require 'lib.redis_retry'
-
-local red = redis_retry.new('127.0.0.1', 6379, 3)
-
-print(red:set('ni', 'hao'))
-print(red:get('ni'))
