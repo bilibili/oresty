7822ac192df26400711f0e6467e9e73c30df4cd9
tangjunxing <tangjunxing@bilibili.com>
2020-03-13 17:12:59 +0800

    Mod

--- check-env.sh
+++ check-env.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e
 
-test -e config || ln -s config_${1:-dev} config
-
-dpkg -i ./install.deb
-
 NAME=${1:-oresty}
 
+test -e config || ln -s config_${1:-dev} config
+
+#
+# 安装
+#
+dpkg -i ./install.deb
 mkdir -p /usr/local/${NAME}/site/ && cp -r ./lualib/ /usr/local/${NAME}/site/
 
-install <(sed "s@_APP_DIR_@$PWD@;s@_NAME_@$name@" init.template) /etc/init.d/${NAME}
+#
+# 配置systemd
+#
+cp <(sed "s@_APP_DIR_@$PWD@;s@_NAME_@$NAME@" oresty.service) /etc/systemd/system/${NAME}.service
+systemctl enable ${NAME}
 
 test -e .git && {
     test -e .git/hooks/pre-commit || {
